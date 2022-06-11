2551e950029745cc29fd51c71d10914a4fb6b75f
root <100w@users.noreply.github.com>
2022-06-11 04:54:32 +0300

    Done

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -3,13 +3,12 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/admin")
@@ -23,31 +22,25 @@ public class AdminController {
     }
 
     @GetMapping("")
-    public String findAll(Model model) {
+    public String findAll(Model model, Principal principal) {
+        User user = userService.findByUsername(principal.getName());
+        model.addAttribute("admin", user);
         model.addAttribute("users", userService.findAll());
+        for (Role el : user.getRoles()) {
+            if (el.getName().equals("ROLE_ADMIN")) {
+                model.addAttribute("role", "ADMIN");
+                break;
+            }
+            if (el.getName().equals("ROLE_USER")) {
+                model.addAttribute("role", "USER");
+                break;
+            }
+        }
         return "admin";
     }
 
-    @GetMapping("/user-create")
-    public String createUserForm(User user) {
-        return "user-create";
-    }
-
-    @PostMapping("/user-create")
-    public String createUser(User user) {
-        userService.saveUser(user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/user-update/{id}")
-    public String updateUserForm(@PathVariable("id") long id, Model model) {
-        User user = userService.findById(id);
-        model.addAttribute("user", user);
-        return "user-update";
-    }
-
-    @PostMapping("/user-update")
-    public String updateUser(User user) {
+    @PostMapping("/user-save")
+    public String saveUser(User user) {
         userService.saveUser(user);
         return "redirect:/admin";
     }
@@ -58,4 +51,3 @@ public class AdminController {
         return "redirect:/admin";
     }
 }
-
--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
@@ -21,9 +23,19 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping
+    @GetMapping("")
     public String userInfoPage(Principal principal, Model model) {
         User user = userService.findByUsername(principal.getName());
+        for (Role el : user.getRoles()) {
+            if(el.getName().equals("ROLE_ADMIN")) {
+                model.addAttribute("role", "ADMIN");
+                break;
+            }
+            if (el.getName().equals("ROLE_USER")){
+                model.addAttribute("role", "USER");
+                break;
+            }
+        }
         model.addAttribute("user", user);
         return "user";
     }
